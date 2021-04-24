9821c661300a39defeac87406839be3921d27ae9
Yurii Rashkovskii <yrashk@gmail.com>
2021-04-23 20:51:24 -0700

    Problem: can't do anything but start and end a workflow
    Solution: introduce an abstract task activity
    
    While it's not useful on its own, it's a good building block for
    testing gateways (this should come soon)

---
+++ pkg/flow_node/activity/task/tests/task_test.go
@@ -0,0 +1,55 @@
+package tests
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"bpxe.org/pkg/bpmn"
+	"bpxe.org/pkg/process"
+	"bpxe.org/pkg/tracing"
+
+	_ "github.com/stretchr/testify/assert"
+)
+
+func TestStartEvent(t *testing.T) {
+	var testDoc bpmn.Definitions
+	var err error
+	src, err := testdata.ReadFile("testdata/task.bpmn")
+	if err != nil {
+		t.Fatalf("Can't read file: %v", err)
+	}
+	err = xml.Unmarshal(src, &testDoc)
+	if err != nil {
+		t.Fatalf("XML unmarshalling error: %v", err)
+	}
+	processElement := (*testDoc.Processes())[0]
+	proc := process.NewProcess(&processElement, &testDoc)
+	if instance, err := proc.Instantiate(); err == nil {
+		traces := instance.Tracer.Subscribe()
+		err := instance.Run()
+		if err != nil {
+			t.Fatalf("failed to run the instance: %s", err)
+		}
+	loop:
+		for {
+			trace := <-traces
+			switch trace := trace.(type) {
+			case tracing.FlowTrace:
+				if id, present := trace.Source.Id(); present {
+					if *id == "task" {
+						// success!
+						break loop
+					}
+
+				}
+			case tracing.ErrorTrace:
+				t.Fatalf("%#v", trace)
+			default:
+				t.Logf("%#v", trace)
+			}
+		}
+		instance.Tracer.Unsubscribe(traces)
+	} else {
+		t.Fatalf("failed to instantiate the process: %s", err)
+	}
+}
