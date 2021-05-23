9c0b2143fbe74fa127c679e147c57f409a6fff29
Yurii Rashkovskii <yrashk@gmail.com>
2021-05-23 16:54:59 -0700

    Problem: process instantiation by a recurrent timer
    Not sure if process can be instantiated recurrently by a timer.
    
    Solution: write a test for this use case
    
    It seems to work indeed.

--- pkg/model/tests/timer_instantiation_test.go
+++ pkg/model/tests/timer_instantiation_test.go
@@ -65,3 +65,50 @@ loop1:
 		}
 	}
 }
+
+var testRecurringTimerStartEventInstantiation bpmn.Definitions
+
+func init() {
+	internal.LoadTestFile("testdata/instantiate_recurring_timer_start_event.bpmn", testdata, &testRecurringTimerStartEventInstantiation)
+}
+
+func TestRecurringTimerStartEventInstantiation(t *testing.T) {
+	c := clock.NewMock()
+	ctx := clock.ToContext(context.Background(), c)
+	tracer := tracing.NewTracer(ctx)
+	traces := tracer.SubscribeChannel(make(chan tracing.Trace, 128))
+	m := model.New(&testRecurringTimerStartEventInstantiation, model.WithContext(ctx), model.WithTracer(tracer))
+	err := m.Run(ctx)
+	require.Nil(t, err)
+loop:
+	for {
+		select {
+		case trace := <-traces:
+			_, ok := trace.(flow.FlowTrace)
+			// Should not flow
+			require.False(t, ok)
+		default:
+			break loop
+		}
+	}
+	// Test for some arbitrary number of recurrences (say, 10?)
+	for i := 0; i < 10; i++ {
+		// Advance clock by 1M
+		c.Add(1 * time.Minute)
+	loop1:
+		for {
+			trace := <-traces
+			switch trace := trace.(type) {
+			case flow.VisitTrace:
+				if idPtr, present := trace.Node.Id(); present {
+					if *idPtr == "end" {
+						// we've reached the desired outcome
+						break loop1
+					}
+				}
+			default:
+				t.Logf("%#v", trace)
+			}
+		}
+	}
+}
